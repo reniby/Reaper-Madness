578882484f9dc3596ff0d89e5817894336699cce
itaibdr <itaicarl@gmail.com>
2026-04-26 16:11:46 -0400

    menu and tutorial UI

--- scripts/menu.gd
+++ scripts/menu.gd
@@ -1,5 +1,25 @@
 extends Node2D
+@onready var play: Button = $Play
+@onready var tutorial: Button = $Tutorial
+@onready var buttons: Array[Button] = [$Play, $Tutorial]
+@onready var num_options: int = len(buttons)
+@onready var scenes = {
+	$Play: "res://scenes/character_select.tscn", 
+	$Tutorial: "res://scenes/tutorial.tscn"
+}
+var selected = 0
 
+
+func _ready():
+	buttons[selected].grab_focus()
+	
 func _process(_delta):
-	if Input.is_anything_pressed():
-		get_tree().change_scene_to_file("res://scenes/character_select.tscn")
+	for player in range(4):
+		if Input.is_action_just_pressed(Globals.character_input[player]["up"]):
+			selected = posmod(selected - 1, num_options)
+			buttons[selected].grab_focus()
+		elif Input.is_action_just_pressed(Globals.character_input[player]["down"]):
+			selected = (selected + 1) % num_options
+			buttons[selected].grab_focus()
+		elif Input.is_action_just_pressed(Globals.character_input[player]["dash"]):
+			get_tree().change_scene_to_file(scenes[buttons[selected]])
---
+++ scripts/tutorial.gd
@@ -0,0 +1,27 @@
+extends Node2D
+@onready var tutorial_pages = [$Pg1, $Pg2, $Pg3, $Pg4]
+@onready var selected = 0
+@onready var num_options = len(tutorial_pages)
+
+# Called when the node enters the scene tree for the first time.
+func _ready() -> void:
+	tutorial_pages[selected].visible = true
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(_delta: float) -> void:
+	for player in range(4):
+		if Input.is_action_just_pressed(Globals.character_input[player]["right"]):
+			var prev = selected
+			selected = (selected + 1) % num_options
+			tutorial_pages[prev].visible = false
+			tutorial_pages[selected].visible = true
+			
+		elif Input.is_action_just_pressed(Globals.character_input[player]["left"]):
+			var prev = selected
+			selected = posmod(selected - 1, num_options)
+			tutorial_pages[prev].visible = false
+			tutorial_pages[selected].visible = true
+			
+		elif Input.is_action_just_pressed(Globals.character_input[player]["dash"]) and selected == (len(tutorial_pages) - 1):
+			get_tree().change_scene_to_file("res://scenes/character_select.tscn")
