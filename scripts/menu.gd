a5aff3b9e14d709bb674317e6b936935e9e14ba5
itaibdr <itaicarl@gmail.com>
2026-04-26 18:03:36 -0400

    UI beutification

--- scripts/menu.gd
+++ scripts/menu.gd
@@ -16,10 +16,20 @@ func _ready():
 func _process(_delta):
 	for player in range(4):
 		if Input.is_action_just_pressed(Globals.character_input[player]["up"]):
+			var prev = selected
 			selected = posmod(selected - 1, num_options)
 			buttons[selected].grab_focus()
+			for arrow in buttons[prev].get_children():
+				arrow.visible = false
+			for arrow in buttons[selected].get_children():
+				arrow.visible = true
 		elif Input.is_action_just_pressed(Globals.character_input[player]["down"]):
+			var prev = selected
 			selected = (selected + 1) % num_options
 			buttons[selected].grab_focus()
+			for arrow in buttons[prev].get_children():
+				arrow.visible = false
+			for arrow in buttons[selected].get_children():
+				arrow.visible = true
 		elif Input.is_action_just_pressed(Globals.character_input[player]["dash"]):
 			get_tree().change_scene_to_file(scenes[buttons[selected]])
