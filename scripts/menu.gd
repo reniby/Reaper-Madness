ff9658328a24ff7854f7f99182042d85d8b87722
itaibdr <itaicarl@gmail.com>
2026-04-28 16:43:27 -0400

    fade from black on end screen

--- scripts/menu.gd
+++ scripts/menu.gd
@@ -8,6 +8,10 @@ var num_options: int
 var selected = 0
 
 func _ready():
+	if has_node("FadeFromBlack"):
+		var fade_from_black = $FadeFromBlack
+		var tween = create_tween()
+		tween.tween_property(fade_from_black, "color:a", 0, 0.6)
 	num_options = len(buttons)
 	buttons[selected].grab_focus()
 	for i in num_options:
@@ -34,3 +38,6 @@ func _process(_delta):
 		if Input.is_action_just_pressed(Globals.character_input[player]["dash"]):
 			Globals.resetGlobals()
 			get_tree().change_scene_to_file(button_dict[buttons[selected]])
+			if has_node("FadeFromBlack"):
+				var fade_from_black = $FadeFromBlack
+				fade_from_black.color.a = 1
