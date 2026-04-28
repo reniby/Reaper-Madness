5fac71c21830a9d0a8954e63d052d9b2e2ce15b7
itaibdr <itaicarl@gmail.com>
2026-04-27 23:54:41 -0400

    ending boom

--- scripts/catfight.gd
+++ scripts/catfight.gd
@@ -2,10 +2,15 @@ extends Node2D
 
 @export var pickup_scene: PackedScene = preload("res://scenes/pickup.tscn")
 @onready var player_scores: Array[Label] = [$Labels/P1, $Labels/P2, $Labels/P3, $Labels/P4]
+@onready var game_timer: Timer = $GameTimer
+@onready var ready_to_slow: bool = false
+var players = []
+@onready var fade2black: ColorRect = $CosmicBackgroundOuterSpace2
 
 var player_scene: PackedScene = preload("res://scenes/player.tscn")
 var map_options_scene: PackedScene = preload("res://scenes/map_options.tscn")
 var title = "Reaper Madness :D"
+ 
 var player_positions = [
 	Vector2(-120, -30),
 	Vector2(-40,-30),
@@ -38,13 +43,24 @@ func _ready():
 			child.player = i
 			add_child(child)
 			child.global_position = player_positions[i]
+			players.append(child)
 
 func _process(_delta):
 	DisplayServer.window_set_title(title + " | fps: " + str(Engine.get_frames_per_second()))
 	for i in range(len(Globals.scores)):
 		if Globals.players[i]:
 			player_scores[i].text = str(Globals.scores[i])
+	if game_timer.time_left < 0.3 and not ready_to_slow: 
+		fade2black.modulate.a = 1.0
+		ready_to_slow = true
+		for _player in players:
+			_player.death()
+		var tween = create_tween()
+		tween.tween_property(Engine, "time_scale", 0.1, 0.3)
+		tween.tween_property(fade2black, "color:a", 1, 0.3)
 
 func _on_game_timer_timeout() -> void:
+	Engine.time_scale = 1
+	fade2black.color.a = 0.0
 	get_tree().change_scene_to_file("res://scenes/end_screen.tscn")
 	
