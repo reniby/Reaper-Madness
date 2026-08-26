84ca661600d2c5f47cc6fe3a43619bfa116fc1f0
reniby <52298111+reniby@users.noreply.github.com>
2026-08-25 19:39:46 -0500

    timer redden

--- scripts/timer.gd
+++ scripts/timer.gd
@@ -7,3 +7,10 @@ func _process(_delta: float) -> void:
 	if game_timer.time_left:
 		text = str(int(game_timer.time_left)).pad_zeros(2)
 		ms.text = "." + str(int(fmod(game_timer.time_left, 1.0) * 100)).pad_zeros(2)
+		
+		if game_timer.time_left < 15:
+			modulate = Color("#E25545").lerp(Color.WHITE, (game_timer.time_left - 10) / 5)
+		if game_timer.time_left < 10:
+			modulate = Color("#E25545")
+	else:
+		modulate = Color.WHITE
